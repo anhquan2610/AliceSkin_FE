15df4dfd6770ec1031c5a3830b4f90f90866afe4
anhquan2610 <Quanddagch200326@fpt.edu.vn>
2024-11-24 23:05:49 +0700

    fix chart

--- src/pages/managementPages/Charts/BlogStatusChart.jsx
+++ src/pages/managementPages/Charts/BlogStatusChart.jsx
@@ -9,6 +9,7 @@ import {
   Legend,
   ResponsiveContainer,
   LabelList,
+  Label,
 } from "recharts";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllBlogAdmin } from "../../../store/blogSlice";
@@ -25,25 +26,41 @@ export default function BlogStatusChart() {
   useEffect(() => {
     if (status_counts) {
       const data = [
-        { name: "Draft", count: status_counts.draft },
-        { name: "Published", count: status_counts.published },
+        { name: "Draft", count: status_counts.draft, color: "#8884d8" }, 
+        { name: "Published", count: status_counts.published, color: "#82ca9d" }, 
       ];
       setChartData(data);
     }
   }, [status_counts]);
 
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <BarChart data={chartData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="count" fill="#8884d8">
-          <LabelList dataKey="count" position="top" fill="#8884d8" />
-        </Bar>
-      </BarChart>
-    </ResponsiveContainer>
+    <div style={{ textAlign: "center" }}>
+      <h3 style={{ marginBottom: "20px" }}>Blog Status Overview</h3>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={chartData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name">
+            <Label value="Blog Status" offset={-10} position="insideBottom" />
+          </XAxis>
+          <YAxis>
+            <Label
+              value="Number of Blogs"
+              angle={-90}
+              position="insideLeft"
+              style={{ textAnchor: "middle" }}
+            />
+          </YAxis>
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="count">
+            {chartData.map((entry, index) => (
+              <Bar key={`bar-${index}`} dataKey="count" fill={entry.color}>
+                <LabelList dataKey="count" position="top" fill={entry.color} />
+              </Bar>
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
